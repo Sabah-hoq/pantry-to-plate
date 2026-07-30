5a10323ae27907505c1054f0d4980591593c1750
kai-coder <54784121+kai-coder@users.noreply.github.com>
2026-07-30 19:12:27 -0400

    fixed datatyping to object almost blew up ur computah whoopsies

--- main.py
+++ main.py
@@ -6,22 +6,34 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 df = pd.read_parquet("recipes.parquet")
+df["NER"] = df["NER"].apply(lambda x: list(map(lambda y : y[1:], x[1:-2].upper().split("\", "))))
 
 def get_all_ingredients(df):
-    df["NER"] = df["NER"].apply(lambda x: list(map(lambda y : y[1:], x[1:-2].upper().split("\", "))))
-    np.save("ingredients.npy", np.concat(df["NER"]))
+    np.save("ingredients.npy", np.concat(df["NER"], dtype=object))
+    return df["NER"]
 
-get_all_ingredients(df)
+#ingredients = get_all_ingredients(df)
 
-ingredient = np.load("ingredients.npy", allow_pickle=True)
-values, counts = np.unique_counts(ingredient)
+def get_all_categories():
+    ingredients = np.load("ingredients.npy", allow_pickle=True)
+    values, counts = np.unique_counts(ingredients)
 
-mask = np.fromiter(map(len, values), dtype=int) > 2
-values, counts = values[mask], counts[mask]
+    mask = np.fromiter(map(len, values), dtype=int) > 2
+    values, counts = values[mask], counts[mask]
 
-percent = counts / len(ingredient)
-mask = percent > 0.0001
-values, counts = values[mask][:-2], counts[mask][:-2]
-print(len(values))
+    percent = counts / len(ingredients)
+    mask = percent > 0.0001
+    values, counts = values[mask][:-2], counts[mask][:-2]
+    values = values.view(object)
+
+    np.save("categories.npy", values)
+
+    return values
+
+#categories = get_all_categories()
+
+categories = np.load("categories.npy", allow_pickle=True)
+
+
 
 
