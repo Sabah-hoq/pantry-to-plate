a52a37ed9688f492d4fb989f4d4eb6bb5ad03cd7
kai-coder <54784121+kai-coder@users.noreply.github.com>
2026-07-31 17:43:42 -0400

    added very unclean training code

--- main.py
+++ main.py
@@ -3,9 +3,15 @@
 import pandas as pd
 import pyarrow
 import numpy as np
+from special import spiceData, spiceModel
 import matplotlib.pyplot as plt
+from sympy.geometry.entity import translate
+import torch.nn as nn
+import torch.optim as optim
+import torch
+from tqdm import tqdm
 
-# Uncommet lines below to get .parquet file 
+# Uncomment lines below to get .parquet file
 # df = pd.read_csv('recipes.csv', low_memory=False)
 # df.to_parquet('recipes.parquet', index=False)
 
@@ -34,7 +40,7 @@ def get_all_categories():
 
     return values
 
-categories = get_all_categories()
+#categories = get_all_categories()
 
 categories = np.load("categories.npy", allow_pickle=True)
 category_dict = dict(zip(categories, range(len(categories))))
@@ -43,7 +49,70 @@ df["NER"] = df["NER"].apply(lambda x: list(map(lambda y: category_dict[y] if y i
 df["compromised"] = df["NER"].apply(lambda x: (np.array(x) == -1).mean())
 df = df[(df["compromised"] < 0.2).values]
 
-print(df["NER"])
+train_data = np.zeros((len(df), 790),dtype=bool)
+
+train_data[np.arange(len(df)).repeat(df["NER"].apply(len).values), np.concat(df["NER"].values, dtype=int)] = True
+
+train_data = train_data[:, :-1]
+np.random.shuffle(train_data)
+
+index_max = int(len(train_data) * 0.75)
+train = spiceData(train_data[:index_max], batch_size=512, train=True)
+test = spiceData(train_data[index_max:], batch_size=512, train=False)
+
+model = spiceModel()
+
+def print_similar_categories(trained_model):
+    trained_model.load_state_dict(torch.load("model_weights.pt"))
+    trained_model.eval()
+
+    total = nn.functional.softmax(trained_model(torch.eye(789,dtype=torch.float32)), dim=1)
+
+    sorted_arg = torch.argsort(total, dim=1, descending=True)
+    sorted_output = torch.sort(total, dim=1, descending=True)[0][:, :10]
+
+    similar_categories = categories[torch.flatten(sorted_arg[:, :10])].reshape(-1, 10)
+
+    print(sorted_output)
+    print(similar_categories)
+
+#print_similar_categories(model)
+#STOP
+
+loss_funct = nn.CrossEntropyLoss()
+optimizer = optim.Adagrad(model.parameters())
+scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=8, gamma=0.1)
+
+for epoch in range(50):
+    print("epoch:", epoch)
+    train_loss = 0
+    test_loss = 0
+    model.train()
+    for x, y in tqdm(train):
+        optimizer.zero_grad()
+
+        pred = model(x)
+        loss = loss_funct(pred, y)
+        train_loss += loss.item()
+
+        loss.backward()
+        optimizer.step()
+    print("train loss:", train_loss / train.__len__())
+
+    model.eval()
+    with torch.no_grad():
+        for x, y in tqdm(test):
+            pred = model(x)
+            loss = loss_funct(pred, y)
+            test_loss += loss.item()
+    print("test loss:", test_loss / test.__len__())
+
+    train.on_epoch_end()
+    test.on_epoch_end()
+    scheduler.step()
+    print()
+
+torch.save(model.state_dict(), 'model_weights.pt')
 
 
 
---
+++ special.py
@@ -0,0 +1,58 @@
+from torch.utils.data import Dataset
+import numpy as np
+import torch
+import torch.nn as nn
+
+class spiceData(Dataset):
+    def __init__(self, x, batch_size=32, train=True):
+        self.x = x
+        self.temp_x = None
+        self.y = None
+        self.batch_size = batch_size
+        self.train = train
+
+        self.on_epoch_end()
+
+    def __len__(self):
+        return len(self.x) // self.batch_size
+
+    def on_epoch_end(self):
+        if self.y is None or self.train:
+            np.random.shuffle(self.x)
+            self.temp_x = self.x.copy()
+            self.y = np.zeros_like(self.x, dtype=bool)
+
+            axis0_counts = self.x.sum(axis=1)
+            axis0_bin_min_index = np.cumsum(self.x.sum(axis=1)) - axis0_counts
+            index_use = axis0_bin_min_index + np.random.randint(low=0, high=axis0_counts)
+            axis0, axis1 = np.where(self.x)
+
+            self.temp_x[range(len(self.x)), axis1[index_use]] = False
+            self.y[range(len(self.x)), axis1[index_use]] = True
+
+    def get_batch(self, idx):
+        if idx >= self.__len__():
+            raise IndexError()
+
+        idx1 = idx * self.batch_size
+        idx2 = (idx + 1) * self.batch_size
+        return self.temp_x[idx1:idx2], self.y[idx1:idx2]
+
+    def __getitem__(self, idx):
+        x, y = self.get_batch(idx)
+
+        return torch.from_numpy(x).to(torch.float32), torch.from_numpy(y).to(torch.float32)
+
+
+class spiceModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.linear1 = torch.nn.Linear(789, 256)
+        self.linear2 = torch.nn.Linear(256, 789)
+
+
+    def forward(self, x):
+        x = self.linear1(x)
+        x = self.linear2(x)
+        return x
